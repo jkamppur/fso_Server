9fce824aba5a93959d5d8a97f3c167f7a12bb602
Juha Kamppuri <jkamppur@iki.fi>
2024-12-17 00:38:58 +0200

    harj 3.3 3.4 3.5 3.6 ready

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())  // json parser
+
 let persons = [
     {
         "id": "1",
@@ -24,8 +26,6 @@ let persons = [
       }
   ]
 
-app.use(express.json())
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -36,10 +36,55 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${person_count} people</p><p> ${date} </p>`)
 })
 
-
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    response.json(person)
+  })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random()*10000)
+}
+ 
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+
+    if (!person.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+
+    if (!person.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+      }
+  
+    if (persons.filter(person2 => person2.name === person.name).length > 0) {
+      return response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+
+    person.id = String(generateId())
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
