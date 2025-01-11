6eefafe850a863761ec8ece02d9a7e02a4e2ff50
Juha Kamppuri <jkamppur@iki.fi>
2025-01-11 21:51:47 +0200

    Test 3.19, 3.20, 3,21 ready

--- index.js
+++ index.js
@@ -81,17 +81,19 @@ app.post('/api/persons', (request, response, next) => {
     newPerson.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
+
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
 
-  const person = {
-    name: body.content,
-    number: body.number,
-  }
+  const { name, number } = request.body
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number},
+    { new: true, runValidators: true } // context: 'query' }
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -101,19 +103,17 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 // Error handler middelware:
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  console.error(error.name)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  }  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
 // tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -15,8 +15,22 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) { // ^(&|(\d{2}-\d{5,}$)|(\d{3}-\d{4,}$))
+        return /^(&|(\d{2}-\d{5,}$)|(\d{3}-\d{4,}$))/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+
+  },
 })
 
 personSchema.set('toJSON', {  // Will be called automatically, for each person read from database
